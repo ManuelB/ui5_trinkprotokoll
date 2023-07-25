9fc035521bfdc22d5ab36753e03300b0dc082cb2
Manuel Blechschmidt <manuel.blechschmidt@gmx.de>
2023-07-25 12:28:18 +0000

    Added all UI5 software from workshop

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,190 @@
+sap.ui.define([
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/m/library",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+], function (BaseController, JSONModel, formatter, mobileLibrary, Filter, FilterOperator) {
+    "use strict";
+
+    // shortcut for sap.m.URLHelper
+    var URLHelper = mobileLibrary.URLHelper;
+
+    return BaseController.extend("ummtrinkprotokoll.ui5trinkprotokoll.controller.Detail", {
+
+        formatter: formatter,
+
+        /* =========================================================== */
+        /* lifecycle methods                                           */
+        /* =========================================================== */
+
+        onInit: function () {
+            // Model used to manipulate control states. The chosen values make sure,
+            // detail page is busy indication immediately so there is no break in
+            // between the busy indication for loading the view's meta data
+            var oViewModel = new JSONModel({
+                busy : false,
+                delay : 0,
+                lineItemListTitle : this.getResourceBundle().getText("detailLineItemTableHeading")
+            });
+
+            this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
+
+            this.setModel(oViewModel, "detailView");
+
+        },
+
+        /* =========================================================== */
+        /* event handlers                                              */
+        /* =========================================================== */
+
+        /**
+         * Event handler when the share by E-Mail button has been clicked
+         * @public
+         */
+        onSendEmailPress: function () {
+            var oViewModel = this.getModel("detailView");
+
+            URLHelper.triggerEmail(
+                null,
+                oViewModel.getProperty("/shareSendEmailSubject"),
+                oViewModel.getProperty("/shareSendEmailMessage")
+            );
+        },
+
+        
+        /**
+         * Updates the item count within the line item table's header
+         * @param {object} oEvent an event containing the total number of items in the list
+         * @private
+         */
+        onListUpdateFinished: function (oEvent) {
+            var sTitle,
+                iTotalItems = oEvent.getParameter("total"),
+                oViewModel = this.getModel("detailView");
+
+            // only update the counter if the length is final
+            if (this.byId("lineItemsList").getBinding("items").isLengthFinal()) {
+                if (iTotalItems) {
+                    sTitle = this.getResourceBundle().getText("detailLineItemTableHeadingCount", [iTotalItems]);
+                } else {
+                    //Display 'Line Items' instead of 'Line items (0)'
+                    sTitle = this.getResourceBundle().getText("detailLineItemTableHeading");
+                }
+                oViewModel.setProperty("/lineItemListTitle", sTitle);
+            }
+        },
+
+        /* =========================================================== */
+        /* begin: internal methods                                     */
+        /* =========================================================== */
+
+        /**
+         * Binds the view to the object path and expands the aggregated line items.
+         * @function
+         * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+         * @private
+         */
+        _onObjectMatched: function (oEvent) {
+            var sObjectId =  oEvent.getParameter("arguments").objectId;
+            this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
+            this.getView().byId("lineItemsList").getBinding("items").filter(new Filter("patient", FilterOperator.Equals, sObjectId));
+            
+        },
+
+        /**
+         * Binds the view to the object path. Makes sure that detail view displays
+         * a busy indicator while data for the corresponding element binding is loaded.
+         * @function
+         * @param {string} sObjectPath path to the object to be bound to the view.
+         * @private
+         */
+        _bindView: function (sObjectPath) {
+            // Set busy indicator during view binding
+            var oViewModel = this.getModel("detailView");
+
+            // If the view was not bound yet its not busy, only if the binding requests data it is set to busy again
+            /*oViewModel.setProperty("/busy", false);
+
+            this.getView().bindElement({
+                path : sObjectPath,
+                events: {
+                    change : this._onBindingChange.bind(this),
+                    dataRequested : function () {
+                        oViewModel.setProperty("/busy", true);
+                    },
+                    dataReceived: function () {
+                        oViewModel.setProperty("/busy", false);
+                    }
+                }
+            });*/
+        },
+
+        _onBindingChange: function () {
+            var oView = this.getView(),
+                oElementBinding = oView.getElementBinding();
+
+            // No data for the binding
+            if (!oElementBinding.getBoundContext()) {
+                this.getRouter().getTargets().display("detailObjectNotFound");
+                // if object could not be found, the selection in the list
+                // does not make sense anymore.
+                return;
+            }
+
+            var sPath = oElementBinding.getPath(),
+                oResourceBundle = this.getResourceBundle(),
+                oObject = oView.getModel().getObject(sPath),
+                sObjectId = oObject.CategoryID,
+                sObjectName = oObject.CategoryID,
+                oViewModel = this.getModel("detailView");
+
+            this.getOwnerComponent().oListSelector.selectAListItem(sPath);
+
+            oViewModel.setProperty("/shareSendEmailSubject",
+                oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
+            oViewModel.setProperty("/shareSendEmailMessage",
+                oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+        },
+
+        /**
+         * Set the full screen mode to false and navigate to list page
+         */
+        onCloseDetailPress: function () {
+            this.getModel("appView").setProperty("/actionButtonsInfo/midColumn/fullScreen", false);
+            // No item should be selected on list after detail page is closed
+            this.getRouter().navTo("list");
+        },
+
+        /**
+         * Toggle between full and non full screen mode.
+         */
+        toggleFullScreen: function () {
+            var bFullScreen = this.getModel("appView").getProperty("/actionButtonsInfo/midColumn/fullScreen");
+            this.getModel("appView").setProperty("/actionButtonsInfo/midColumn/fullScreen", !bFullScreen);
+            if (!bFullScreen) {
+                // store current layout and go full screen
+                this.getModel("appView").setProperty("/previousLayout", this.getModel("appView").getProperty("/layout"));
+                this.getModel("appView").setProperty("/layout", "MidColumnFullScreen");
+            } else {
+                // reset to previous layout
+                this.getModel("appView").setProperty("/layout",  this.getModel("appView").getProperty("/previousLayout"));
+            }
+        }, onsave:function(){
+            const wert=this.getView().byId("wert").getValue();
+            const code=this.getView().byId("code").getValue();
+            this.getView().getModel().create("Observation",{
+                code:{
+                    coding:[{
+                        code:code
+                    }]
+                }, valueString:wert
+            });
+            this.getView().getModel().submitChanges();
+
+        }
+
+    });
+
+});
---
+++ webapp/controller/DetailObjectNotFound.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+    "./BaseController"
+], function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("ummtrinkprotokoll.ui5trinkprotokoll.controller.DetailObjectNotFound", {});
+});
